a3f262d10cb994760794d5824907df086fa2299f
nej1044 <nej1044@naver.com>
2023-01-23 17:37:45 +0900

    07-class-with-style

---
+++ class/07-class-with-style/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello!
+  </h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // color: 'orange',
+            // fontSize: '30px'
+            fontStyle: {
+                color: 'orange',
+                fontSize: '30px'
+            },
+            backgroundStyle: {
+                backgroundColor: 'black'
+            }
+        }
+    },
+    methods: {
+        changeStyle() {
+            this.fontStyle.color = 'red',
+            this.fontStyle.fontSize = '50px'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.active {
+    color: red;
+    font-weight: bold;
+}
+</style>
