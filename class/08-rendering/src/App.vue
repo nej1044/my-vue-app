d911e0321f9271a6126e9dcdcafcf3388ef9058e
nej1044 <nej1044@naver.com>
2023-01-23 17:55:25 +0900

    08-조건부 렌더링

---
+++ class/08-rendering/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="handler">
+    Click me!
+  </button>
+  <h1 v-show="isShow">
+    Hello
+  </h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isShow: false,
+            count: 0
+        }
+    },
+    methods: {
+        handler() {
+            this.isShow = !this.isShow
+            this.count++;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
