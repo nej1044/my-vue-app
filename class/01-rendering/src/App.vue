f1f3d1f76845a6450644ff34b83e284f0eb5ccc4
nej1044 <nej1044@naver.com>
2023-01-23 16:13:20 +0900

    02-if, for

--- class/01-rendering/src/App.vue
+++ class/01-rendering/src/App.vue
@@ -2,13 +2,29 @@
   <h1 @click="increase">
     {{ count }}
   </h1>
+  <div v-if="count > 4">
+    4보다 큽니다!
+  </div>
+  <ul>
+    <Fruit
+      v-for="fruit in fruits"
+      :key="fruit"
+      :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
 </template>
 
 <script>
+import Fruit from '~/components/Fruit';
     export default {
+        components: {
+            Fruit
+        },
         data() {
             return {
-                count: 0
+                count: 0,
+                fruits: ['Apple', 'Banana', 'Cherry']
             }
         },
         methods: {
@@ -19,9 +35,14 @@
     }
 </script>
 
-<style>
+<style lang="scss">
     h1 {
         font-size: 50px;
         color: royalblue;
+    }
+    ul {
+        li {
+            font-size: 40px;
+        }
     }
 </style>
