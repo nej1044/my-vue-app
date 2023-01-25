36cc9c38f3535c4cd02abc784e2b54987e267f5c
nej1044 <nej1044@naver.com>
2023-01-25 16:58:01 +0900

    composition-api

---
+++ class/composition-api/src/App.Composition.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1 @click="increase">
+    {{ count }} // {{ doublecount }}
+  </h1>
+  <h1 @click="changeMessage">
+    {{ message }} // {{ reversedMessage }}
+  </h1>
+</template>
+
+<script>
+import {ref, computed, watch, onMounted} from 'vue'
+
+export default {
+    setup(){
+        const count = ref(0)
+        const message = ref('Hello World!!')
+
+        const doublecount = computed(()=> count.value * 2)
+        const reversedMessage = computed(()=> message.value.split('').reverse().join(''))
+
+        watch(message, newValue => {
+            console.log(newValue)
+        })
+
+        const increase=()=> count.value++
+        const changeMessage =()=> message.value = 'Good!!'
+
+        onMounted(()=> {
+            console.log(count.value)
+        })
+
+        console.log(message.value)
+
+        return {count, message, increase, changeMessage, doublecount, reversedMessage}
+    }
+}
+</script>
---
+++ class/composition-api/src/components/MyBtn.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="btn"
+    v-bind="$attrs"
+    @click="hello">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import {onMounted} from 'vue'
+export default {
+    inheritAttrs: false,
+    props: {
+        color: {
+            type: String,
+            default: 'gray'
+        }
+    },
+    emits: ['hello'],
+    setup(props, context){  
+        const hello=()=>{
+            context.emit('hello')
+        }
+        onMounted(()=> {
+            console.log(props.color)
+            console.log(context.attrs)
+        })
+
+        return { hello }
+    }
+}
+</script>
