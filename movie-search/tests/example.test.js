b45bf0c9a28ba2c35ff924dd0a3a62ffb1aed94b
nej1044 <nej1044@naver.com>
2023-02-06 17:33:57 +0900

    add: 첫 컴포넌트 테스트

--- movie-search/tests/example.test.js
+++ movie-search/tests/example.test.js
@@ -1,18 +1,13 @@
-import axios from "axios";
-import { fetchMovieTitle } from "./example";
+import { mount } from "@vue/test-utils";
+import Example from "./Example.vue";
 
-describe('비동기 테스트', ()=> {
-    test('영화 제목 변환', async () => {
-        axios.get = jest.fn(()=> {
-            return new Promise(resolve => {
-                resolve({
-                    data: {
-                        Title: 'Frozen II'
-                    }
-                })
-            })
-        })
-        const title = await fetchMovieTitle()
-        expect(title).toBe('Frozen ii')
+test('메세지를 변경합니다.', async () => {
+    const wrapper = mount(Example)
+    expect(wrapper.vm.msg).toBe('Hello Vue test utils!!')
+    // wrapper.vm.msg = 'Hello GRACE!!'
+    await wrapper.setData({
+        msg: 'Hello GRACE!!'
     })
+    expect(wrapper.vm.msg).toBe('Hello GRACE!!')
+    expect(wrapper.find('div').text()).toBe('Hello GRACE??')
 })
