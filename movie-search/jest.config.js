a810338081aeae508c5d6ffed184b7792890d8f4
nej1044 <nej1044@naver.com>
2023-02-06 22:54:25 +0900

    add: Search VTU

--- movie-search/jest.config.js
+++ movie-search/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
         '<rootDir>/node_modules/',
         '<rootDir>/dist'
     ],
+    testEnvironment: 'jsdom',
     // jsdom 환경에 대한 URL 설정
     testURL: 'http://localhost',
     // 정규식과 일치하는 파일의 변환 모듈 지정
